bcd85851677a0ffa42cb260620afd4300cfdd6c5
Ajay dayma <32808481+ajaysbugatti@users.noreply.github.com>
2019-03-31 11:50:04 +0530

    Update Dining_philosopher.cpp

--- Dining_philosopher.cpp
+++ Dining_philosopher.cpp
@@ -1,8 +1,14 @@
+/*   17114006
+Ajay Dayma
+cse 2nd yr
+*/
+
+#include <random>
+#include <unistd.h>
 #include <iostream>
 #include <pthread.h>
 #include <semaphore.h>
-#include <random>
-#include <unistd.h>
+
 
 #define PHILOSOPHERS 5
 #define LEFT (philosopher_num + 4) % PHILOSOPHERS
@@ -26,12 +32,12 @@ void *philospher(void *arg);
 
 int main(){
     pthread_t philospher_thread[PHILOSOPHERS];
-    // initialize the semaphores
+    
     sem_init(&lock, 0, 1);
 
     for(int i = 0; i < PHILOSOPHERS; i++)
         sem_init(&S[i], 0, 0);
-    // create philosopher processes
+   
     for (int i = 0; i < PHILOSOPHERS; i++) {
         pthread_create(&philospher_thread[i], NULL, philospher, (void *)(intptr_t)i);
         cout << "Philosopher " << i+1 << "is thinking" << endl;
@@ -41,7 +47,7 @@ int main(){
 
 void check(int philosopher_num){
     if (state[philosopher_num] == HUNGRY && state[LEFT] != EATING && state[RIGHT] != EATING){
-        // state that eating
+        
         state[philosopher_num] = EATING;
         sleep(rand()%4);
         cout << "Philosopher " << philosopher_num+1 << " takes fork " << LEFT+1 << " and " << philosopher_num+1 << endl;
@@ -50,24 +56,24 @@ void check(int philosopher_num){
     }
 }
 
-// take up chopsticks
+
 void take_chopstick(int philosopher_num){
     sem_wait(&lock);
-    // state that hungry
+   
     state[philosopher_num] = HUNGRY;
     cout << "Philosopher " << philosopher_num+1 << " is Hungry" << endl;
-    // eat if neighbours are not eating
+    
     check(philosopher_num);
     sem_post(&lock);
-    // if unable to eat wait to be signalled
+    
     sem_wait(&S[philosopher_num]);
     sleep(rand()%3);
 }
 
-// put down chopsticks
+
 void put_chopstick(int philosopher_num){
     sem_wait(&lock);
-    // state that thinking
+    
     state[philosopher_num] = THINKING;
     cout << "Philosopher " << philosopher_num+1 << " putting fork " << LEFT+1 << " and " << philosopher_num+1 <<  " down" << endl;
     cout << "Philosopher " << philosopher_num+1 << "is thinking" << endl;
